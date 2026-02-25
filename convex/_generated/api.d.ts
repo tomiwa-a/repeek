dc3605900d8e18b120649f3ea36fe2623133f0ba
Tomiwa <tomiwamole@gmail.com>
2026-02-25 12:43:22 +0100

    setup auth

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,6 +10,7 @@
 
 import type * as auth from "../auth.js";
 import type * as games from "../games.js";
+import type * as http from "../http.js";
 import type * as lib_oddsApi from "../lib/oddsApi.js";
 import type * as sports from "../sports.js";
 
@@ -22,6 +23,7 @@ import type {
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   games: typeof games;
+  http: typeof http;
   "lib/oddsApi": typeof lib_oddsApi;
   sports: typeof sports;
 }>;
