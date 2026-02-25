f30e349c9775112ac6ab5c9b0da6116219dfcb2e
Tomiwa <tomiwamole@gmail.com>
2026-02-25 12:24:49 +0100

    setup auth tables

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,8 @@
+import { Password } from "@convex-dev/auth/providers/Password";
 import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Google],
+  providers: [Password, Google],
 });
 
