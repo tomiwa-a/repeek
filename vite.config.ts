9070f686fb716720c6c51598517305445a908cde
Tomiwa <tomiwamole@gmail.com>
2026-02-26 11:31:59 +0100

    adde dngrok to vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["c5b0-98-97-77-49.ngrok-free.app"]
+  }
 });
